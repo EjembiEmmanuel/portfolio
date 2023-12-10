5e8b981d3da4d868acbe0343364b956178eecd3d
EjembiEmmanuel <ejembioche50@gmail.com>
2023-12-10 17:43:23 +0100

    Created Navbar for tablets and phones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,16 @@ import { useState, useEffect } from "react";
 import {
     NavLink,
 } from "react-router-dom"
+import { useMediaQuery } from "react-responsive";
+import menuIcon from '../assets/icons/menu.svg'
 import styles from '../scss/Navbar.module.scss'
 
 
 const Navbar = () => {
     const [navbarColor, setNavbarColor] = useState(false);
+    const [isNavActive, setNavActive] = useState(false)
+    const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)'})
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
 
     const changeNavbarColor = () => {
         if(window.scrollY >= 80) {
@@ -15,6 +20,10 @@ const Navbar = () => {
             setNavbarColor(false);
         }
     };
+
+    const toggleNav = () => {
+        setNavActive(!isNavActive)
+    }
 
     useEffect(() => {
         window.addEventListener('scroll', changeNavbarColor);
@@ -38,22 +47,47 @@ const Navbar = () => {
         <nav style={{boxShadow: navbarColor ? '5px 5px 15px rgba(0,0,0,0.3)' : ''}}>
             <div id={styles['logo-container']}>
                 <p id={styles['logo-text']}>Oche Codes</p>
+                {isTabletOrMobile && (
+                    <div>
+                        <button onClick={toggleNav}>
+                            <img src={menuIcon} alt="Menu icon" />
+                        </button>
+                    </div>
+                )}
             </div>
             <div id={styles['links-container']}>
-                <div id={styles['links']}>
-                    <NavLink className={({ isActive, isPending }) =>
-                        addNavClass(isActive, isPending)
-                    } to='/'>Home</NavLink>
-                    <NavLink className={({ isActive, isPending }) =>
-                        addNavClass(isActive, isPending)
-                    } to='about'>About</NavLink>
-                    <NavLink className={({ isActive, isPending }) =>
-                        addNavClass(isActive, isPending)
-                    } to='portfolio'>Portfolio</NavLink>
-                    <NavLink className={({ isActive, isPending }) =>
-                        addNavClass(isActive, isPending)
-                    } to='resume'>Resume</NavLink>
-                </div>
+                {isDesktopOrLaptop && (
+                    <div id={styles['links']}>
+                        <NavLink className={({ isActive, isPending }) =>
+                            addNavClass(isActive, isPending)
+                        } to='/'>Home</NavLink>
+                        <NavLink className={({ isActive, isPending }) =>
+                            addNavClass(isActive, isPending)
+                        } to='about'>About</NavLink>
+                        <NavLink className={({ isActive, isPending }) =>
+                            addNavClass(isActive, isPending)
+                        } to='portfolio'>Portfolio</NavLink>
+                        <NavLink className={({ isActive, isPending }) =>
+                            addNavClass(isActive, isPending)
+                        } to='resume'>Resume</NavLink>
+                    </div>
+                )}
+                {isNavActive && (
+                    <div id={styles['links']}>
+                        <NavLink onClick={() => setNavActive(false)} className={({ isActive, isPending }) =>
+                            addNavClass(isActive, isPending)
+                        } to='/'>Home</NavLink>
+                        <NavLink onClick={() => setNavActive(false)} className={({ isActive, isPending }) =>
+                            addNavClass(isActive, isPending)
+                        } to='about'>About</NavLink>
+                        <NavLink onClick={() => setNavActive(false)} className={({ isActive, isPending }) =>
+                            addNavClass(isActive, isPending)
+                        } to='portfolio'>Portfolio</NavLink>
+                        <NavLink onClick={() => setNavActive(false)} className={({ isActive, isPending }) =>
+                            addNavClass(isActive, isPending)
+                        } to='resume'>Resume</NavLink>
+                    </div>
+                )}
             </div>
         </nav>
     )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,8 +15,8 @@ const About = () => {
             <div id={styles['about-page']}>
                 <div id={styles['about']}>
                     <p>Hi there! I&apos;m <span>Ejembi Emmanuel</span>, a passionate <span>Web developer</span> based in <span>Nigeria.</span></p>
-                    <p>Throughout childhood, I was always fascinated by technology. Whether it was jailbreaking my first android phone, installing custom ROMs on my Android phones (which almost resulted in them becoming inoperable), or modding games, these activities were instrumental in enhancing my ability to solve problems.</p>
-                    <p>In my spare time, I enjoy learning new technologies and exploring the latest web development trends. Im&apos; always eager to learn and grow, and I&apos;m currently delving deeper into the world of Front-End Web Development.</p>
+                    <p>I&apos;ve over 3 years of development experience in which I&apos;ve not only learnt to code using a variety of tools and frameworks but also learnt how to solve and provide intuitive solutions to problems, communicate effectively and work in a team. I currently work with JavaScript and React.js on most of my projects.</p>
+                    <p>In my spare time, I enjoy learning new technologies and exploring the latest web development trends. I&apos;m always eager to learn and grow, and I&apos;m currently delving deeper into the world of Front-End Web Development.</p>
                     <p>Feel free to browse through my portfolio to see the work I&apos;ve done, and don&apos;t hesitate to get in touch if you&apos;d like to collaborate on a project or learn more about my skills and experience.</p>
                 </div>
                 <EmblaCarousel slides={SLIDES} options={OPTIONS} skills={skills} />
